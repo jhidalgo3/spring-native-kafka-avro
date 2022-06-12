be88d8b60e33bea23fc5deaa13186c8927c91d38
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2022-06-12 10:52:45 +0200

    First version spring-native avro and kafka

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 sdk use java 22.0.0.2.r17-grl 
 
 mvn clean package -P default,spring-native,build-native-image
+
+
+__Run spring-boot application__
+
+ mvn spring-boot:run -Dspring-boot.run.profiles=confluent-cloud -P default,spring-native
+
+
+## Build docker Image
+
+mvn clean package -P default,spring-native,build-docker-image
+
+**Specific configuration dir**
+
+SPRING_CONFIG_NAME=application
+SPRING_CONFIG_LOCATION=file:///config/
+SPRING_PROFILES_ACTIVE=docker
+
+docker run -p 8080:8080 -e SPRING_PROFILES_ACTIVE=docker -e SPRING_CONFIG_LOCATION=file:///config/  -ti -v $PWD/src/main/resources/:/config docker.io/jhidalgo3/spring-native-kafka-avro:latest
+
+**Confluent-cloud**
+
+ docker run -p 8080:8080 -e SPRING_PROFILES_ACTIVE=confluent-cloud -e SPRING_CONFIG_LOCATION=file:///config/  -ti -v $PWD/config/:/config docker.io/jhidalgo3/spring-native-kafka-avro:latest
--- src/main/java/com/sample/config/AotConfig.java
+++ src/main/java/com/sample/config/AotConfig.java
@@ -1,15 +1,13 @@
 package com.sample.config;
 
-import com.sample.avro.Address;
-import io.confluent.kafka.schemaregistry.client.rest.entities.ErrorMessage;
-import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaString;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.serializers.subject.TopicNameStrategy;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.security.authenticator.AbstractLogin;
+import org.apache.kafka.common.security.authenticator.AbstractLogin.DefaultLoginCallbackHandler;
+import org.apache.kafka.common.security.authenticator.DefaultLogin;
+import org.apache.kafka.common.security.authenticator.SaslClientCallbackHandler;
+import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.errors.DefaultProductionExceptionHandler;
 import org.apache.kafka.streams.errors.LogAndFailExceptionHandler;
-
 import org.apache.kafka.streams.processor.FailOnInvalidTimestamp;
 import org.apache.kafka.streams.processor.internals.StreamsPartitionAssignor;
 import org.apache.kafka.streams.processor.internals.assignment.FallbackPriorTaskAssignor;
@@ -20,10 +18,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
 
+import com.sample.avro.Address;
+
+import io.confluent.kafka.schemaregistry.client.rest.entities.ErrorMessage;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaString;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.subject.TopicNameStrategy;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+
 @Configuration
 @NativeHint(
         types = @TypeHint(types = {
-
+        		PlainLoginModule.class,
+        		DefaultLogin.class,
+        		AbstractLogin.class,
+        		DefaultLoginCallbackHandler.class,
+        	SaslClientCallbackHandler.class,
                 StreamsPartitionAssignor.class,
                 DefaultProductionExceptionHandler.class,
                 FailOnInvalidTimestamp.class,
